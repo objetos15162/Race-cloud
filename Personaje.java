c32e8ce4a142393ecd082dde29b44b0633c06477
Jeld2016 <jeld2014@hotmail.com>
2016-05-22 18:31:24 -0500

    Version 2
    version casi completa

--- Personaje.java
+++ Personaje.java
@@ -1,4 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.LinkedList;
+import java.lang.Class;
 
 
 public class Personaje extends Actor
@@ -7,53 +9,141 @@ public class Personaje extends Actor
      * Act - do whatever the Crab wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public int rota=0;
-    public int disp=0;
-    int posicion=200;
+     private LinkedList <GreenfootImage> imagenes;
+    public int disp=0, pj;
+    int posicion=200, dis=30;
+    
     
     public void act() 
     {
-       movimiento();
+       movimiento(dis);
     }    
-    
-    public int movimiento()
+    /**
+     * se crea una lista de imagenes con los personajes y sus movimientos
+     */
+    public Personaje()
     {
-        int dis=30,x=getX(),y=getY();
+        imagenes = new LinkedList();
+        imagenes.add(new GreenfootImage("Goku1.png"));      //0
+        imagenes.add(new GreenfootImage("Goku2.png"));      //1
+        imagenes.add(new GreenfootImage("Goku3.png")); //2
+        imagenes.add(new GreenfootImage("Goku4.png"));   //3
+        imagenes.add(new GreenfootImage("Arale1.png"));//4
+        imagenes.add(new GreenfootImage("Arale2.png"));//4
+        imagenes.add(new GreenfootImage("Arale3.png"));//4
+        imagenes.add(new GreenfootImage("Arale4.png"));//4
+        imagenes.add(new GreenfootImage("Milk1.png"));//4
+        imagenes.add(new GreenfootImage("Milk2.png"));//4
+        imagenes.add(new GreenfootImage("Milk3.png"));//4
+        imagenes.add(new GreenfootImage("Milk4.png"));//4
+    }
+    
+    /**
+     * se crea el movimiento del personaje y aumenta una variable con su posicion actual
+     * llama los metodos ganaPuntos() y recibeDaño() a demas de cambiar la imagen de mov
+     */
+    public void movimiento(int dis)
+    {
+       int x=getX(),y=getY();
         String cadena = Greenfoot.getKey();
       
-        rota=rota+1;  
         disp=disp+1;
         move(dis);
       if(cadena == "up")
       {
        setLocation(x,y-dis);
-       setImage("Perso2.png");
+       setImage(getImagen(pj+1));
       }
        if(cadena == "down")
       {
        setLocation(x,y+dis);
-       setImage("Perso3.png");
+       setImage(getImagen(pj+2));
       }
-      if(cadena == "space")
+      /*if(cadena == "space")
       {
-       setImage("Perso4.png");
-      }
-      if(rota==11)
-      {
-      setRotation(0);
-      rota=0;
-      }
+       setImage(getImagen(pj+3));
+      }*/
+      
       if(disp==15)
       {
-      setImage("Perso1.png");
+      setImage(getImagen(pj));
       disp=0;
       }
+      //ganaPuntos();
+      //recibeDaño();
+       posicion+=dis;
       
-      Actor c = getOneObjectAtOffset(0, 0, Puntos.class);
+    } 
+    
+    /**
+     * revisa si es que el personaje entra en contacto con una capsula esta desaparesca
+     */
+    public int ganaPuntos(int puntos,int lvl, boolean ex)
+    {
+        int i=puntos; 
+        if(ex)
+        {
+        Actor c = getOneObjectAtOffset(0, 0, Puntos.class);
+         
         if(c != null) {
             getWorld().removeObject(c);
+            i = puntos + (50*lvl);
+            
+            }
         }
-        posicion+=dis;
-      return posicion;
-    } 
+        return i;
+    }
+    
+    /**
+     * revisa si entra en contacto con una bala y disminuira la barra de vida
+     */
+    public int recibeDaño(int vida,boolean ex)
+    {
+        int i;
+        i=vida;
+        if(ex)
+        {
+        Actor b1 = getOneObjectAtOffset(20, 0, Bala.class);
+        if(b1!= null)
+        {
+            getWorld().removeObject(b1);
+            i= vida - 1;
+            setImage(getImagen(pj+3));
+        }
+        
+        Actor o = getOneObjectAtOffset(20, 0, Obstaculo.class);
+        if(o!= null)
+        {
+            getWorld().removeObject(o);
+            i= vida - 1;
+            setImage(getImagen(pj+3));
+        }
+        
+      }
+        
+        return i;
+    }
+    
+    /**
+     * manda la posicion que se creea en movimiento
+     */
+    public int posicion()
+    {
+        return posicion;
+    }
+    
+        /**
+     * Metodo que devolvera una imagen (tipo GreenfootImage)
+     * que contiene nuestra lista de imagenes y sera asignada
+     * a los distintos objetos del escenario.
+     * 
+     * @param int Valor de la posición de la imágen que se
+     * desea obtener.
+     * @return GreenfootImage - Imágen contenida en la Lista
+     * en la posición especificada.
+     */
+    public GreenfootImage getImagen(int n)
+    {
+        return imagenes.get(n);
+    }
 }
